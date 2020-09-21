60eba9c6e48a02903e4ea1edad5c638a3a84964e
jeohyn <42138399+jeohyn@users.noreply.github.com>
2020-09-21 15:59:18 +0900

    Add files via upload

---
+++ machine_learning/reinforcement_learning/Q-learning_frozen_lake_using_random_noise.py
@@ -0,0 +1,88 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from gym.envs.registration import register
+import random as pr
+
+def rargmax(vector): #https://gist.github.com/stober/1943451
+    m=np.amax(vector)
+    indices=np.nonzero(vector==m)[0]
+    return pr.choice(indices)
+
+####Register FrozenLake w/ is_slippery False
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name':'4x4', 'is_slippery':False}
+    )
+
+env=gym.make('FrozenLake-v3')
+
+####learning part
+#initialize table with all zeros
+Q=np.zeros([env.obersation_space.n, env.action_space.n])
+#set learning parameters. numbers of learning
+dis=.99
+num_episodes=2000
+
+#create lists to conain total rewards and steps per episode
+rList=[]
+for i in range(num_episodes):
+    #Reset environment and get first new observation
+    state=env.reset()
+    rAll=0
+    done=False
+
+    '''
+    ##E-greedy
+        
+    e=1./((i//100)+1)
+    #the Q-Table learning algorithm
+    while not done:
+        #choose an action by e greedy
+        if np.random.rand(1)<e:
+            action=env.action_space.sample()
+        else:
+            action=np.argmax(Q[state, :])
+        #get new state and reward from environment
+        new_stae, reward, done, _=env.step(action)
+
+        #update Q-Table with new knowledge using learning rate_using discounted reward
+        Q[state, action]=reward+dis*np.max(Q[new_state,:])
+
+        rAll+=reward
+        state=new_state
+
+    rList.append(rAll)
+    '''
+    
+    #the Q-Table learning algorithm
+    while not done:
+        ##random noise
+        #choose an action by greedily (with noise) picking from Q table
+        action=np.argmax(Q[state, :]+np.random.randn(1, env.action_space.n)/(i+1))
+        
+
+        #get new state and reward from environment
+        new_stae, reward, done, _=env.step(action)
+
+        #update Q-Table with new knowledge using learning rate_using discounted reward
+        Q[state, action]=reward+dis*np.max(Q[new_state,:])
+
+        rAll+=reward
+        state=new_state
+
+    rList.append(rAll)
+
+####print the result
+print("Success rate : "+str(sum(rList)/num_episodes))
+print("Final Q-Table Values")
+print("LEFT DOWN RIGHT UP")
+print(Q)
+plt.bar(range(len(rList)), rList, color="blue")
+plt.show()
+
+
+
+        
+
